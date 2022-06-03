6ce60f9bd3b04a0a7a00f39173c884ab4c16bd57
hkiriama <humphrey.kiriama@gmail.com>
2022-06-03 16:53:05 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  *
  */
+
 int main(void)
 {
-	char a;
-	for (a = 'a'; a <= 'z' ; a++)
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+
+	for (low = 'a'; low <= 'z'; low++)
 	{
-		if (a == 'q' || a == 'e')
-		{
-			continue;
-		}
-		putchar(a);
+		if (low != e && low != q)
+		putchar(low);
 	}
 	putchar('\n');
+
 	return (0);
 }
