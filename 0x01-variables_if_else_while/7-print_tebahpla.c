87ee69809bef4a8fc1689c5c0d87b8a2b2845c96
hkiriama <humphrey.kiriama@gmail.com>
2022-06-06 12:32:53 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - initialization point
+ * Return: 0 (Success)
+ */
+{
+	char	a;
+
+	for	(a = '0'; a <= '9'; a++)
+		putchar(a);
+
+	for	(a = 'a'; a <= 'f'; a++)
+		putchar(a);
+
+	putchar('\n');
+	return (0);
+}
