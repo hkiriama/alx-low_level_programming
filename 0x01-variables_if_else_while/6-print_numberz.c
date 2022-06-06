373c36aafc01d78beea36f08aa28bd237aa2573a
hkiriama <humphrey.kiriama@gmail.com>
2022-06-06 06:11:39 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,21 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- * 
- * Return: 0 (Success)
- *
+ * main - intitialization point
+ * Return: 0
  */
-
 int main(void)
 {
 	int m;
 
-	for (m = '0'; m <= '9'; m++)
+	for (m = '0'; m <='9'; m++)
 	{
 		putchar(m);
 	}
 	putchar('\n');
-
 	return (0);
 }
