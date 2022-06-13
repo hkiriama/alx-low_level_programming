a357513f31d990d22cb4ec48c54a83180cfec36b
hkiriama <humphrey.kiriama@gmail.com>
2022-06-13 06:21:40 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: parameter to check
+ *
+ * Description: checks whether int n is positive or negative
+ * Return: returns 1 and prints + and returns 0 and prints
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
