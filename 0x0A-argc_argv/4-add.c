8ac7185b4a733f3078e2b66a68cf60e4378513dc
hkiriama <humphrey.kiriama@gmail.com>
2022-07-01 05:54:40 -0700

    task 4 addition

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - adds positive numbers
+ *@argc: argument count
+ *@argv: array of pointers to argument strings
+ *Return: always 0
+**/
+int main(int argc, char *argv[])
+{
+	char *c;
+	int sum = 0;
+
+	while (--argc)
+	{
+		for (c = argv[argc]; *c; c++)
+			if (*c > '9' || *c < '0')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		sum += atoi(argv[argc]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
